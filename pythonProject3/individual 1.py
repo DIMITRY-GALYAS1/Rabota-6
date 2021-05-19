e982db0803dfcda3a261cf3f581b44fcaf09df54
marsellka-ivt <lizeq3586@gmail.com>
2021-05-19 15:25:32 +0300

    13

--- pythonProject3/individual 1.py
+++ pythonProject3/individual 1.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     V = list([random.randint(20, 30) for i in range(7)])
     G = []
     i = 0
-    while i < 7:
+    for (u, d, v) in zip(U, D, V):
         G.append((U[i]+D[i]+V[i])/3)
         i += 1
     print("Среднее значение температуры по дням недели: ", G)
